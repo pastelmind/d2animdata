119c75e0ae29d401516e52881496a616e3f4dee9
pastelmind <keepyourhonor@gmail.com>
2020-03-01 06:10:06 +0900

    Merge unpack_block(), unpack_hash_table() into loads()

--- animdata.py
+++ animdata.py
@@ -122,40 +122,6 @@ def pack_record(record: Record) -> bytes:
     )
 
 
-RECORD_COUNT_FORMAT = "<L"
-
-
-def unpack_block(buffer: bytes, offset: int = 0) -> Tuple[List[Record], int]:
-    """Unpacks a block of AnimData records from the `buffer` at `offset`."""
-    (record_count,) = struct.unpack_from(RECORD_COUNT_FORMAT, buffer, offset=offset)
-    record_offset = offset + struct.calcsize(RECORD_COUNT_FORMAT)
-
-    records = []
-    for _ in range(record_count):
-        record, record_size = unpack_record(buffer, offset=record_offset)
-        records.append(record)
-        record_offset += record_size
-
-    return records, record_offset - offset
-
-
-def unpack_hash_table(buffer: bytes, offset: int = 0) -> Tuple[List[List[Record]], int]:
-    """Unpacks a hash table of AnimData records from the `buffer` at `offset`."""
-    blocks = []
-    block_offset = offset
-    for block_index in range(256):
-        block, block_size = unpack_block(buffer, offset=block_offset)
-        for record in block:
-            hash_value = hash_cof_name(record.cof_name)
-            assert block_index == hash_value, (
-                f"Incorrect hash (COF name={record.cof_name}): "
-                f"expected {block_index} but got {hash_value}"
-            )
-        blocks.append(block)
-        block_offset += block_size
-    return blocks, block_offset - offset
-
-
 def sort_records_by_cof_name(blocks: List[List[Record]]) -> List[Record]:
     """Extracts a list of records from hash table blocks, sorted alphabetically
     by COF name.
@@ -168,8 +134,11 @@ def sort_records_by_cof_name(blocks: List[List[Record]]) -> List[Record]:
     )
 
 
+RECORD_COUNT_FORMAT = "<L"
+
+
 def loads(data: bytes) -> List[Record]:
-    """Loads the contents of AnimData.D2 from the a bytes object.
+    """Loads the contents of AnimData.D2 from binary `data`.
 
     Args:
         file:
@@ -178,12 +147,30 @@ def loads(data: bytes) -> List[Record]:
     Returns:
         List of Record objects, sorted alphabetically by COF name.
     """
-    (blocks, all_blocks_size) = unpack_hash_table(data)
-    data_size = len(data)
-    assert all_blocks_size == data_size, (
-        f"File size mismatch: "
-        f"Blocks use {all_blocks_size} bytes, but binary size is {data_size} bytes"
+    blocks = []
+    offset = 0
+    for block_index in range(256):
+        (record_count,) = struct.unpack_from(RECORD_COUNT_FORMAT, data, offset=offset)
+        offset += struct.calcsize(RECORD_COUNT_FORMAT)
+
+        records = []
+        for _ in range(record_count):
+            record, record_size = unpack_record(data, offset=offset)
+            hash_value = hash_cof_name(record.cof_name)
+            assert block_index == hash_value, (
+                f"Incorrect hash (COF name={record.cof_name}): "
+                f"expected {block_index} but got {hash_value}"
+            )
+            records.append(record)
+            offset += record_size
+
+        blocks.append(records)
+
+    assert offset == len(data), (
+        f"Data size mismatch: "
+        f"Blocks use {offset} bytes, but binary size is {len(data)} bytes"
     )
+
     return sort_records_by_cof_name(blocks)
 
 
